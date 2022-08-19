382866feb40f664ff36dd94b77f44149d7e88f98
charschleiffers <charlotte@missingbracket.dev>
2022-08-18 22:55:28 -0700

    finished update

--- javascript/cookies.js
+++ javascript/cookies.js
@@ -1,1 +1,12 @@
-document.getElementbyId("count-el").innerText= 200
+// document.getElementbyId("count").innerText=20
+
+let cookies = document.getElementById("cookies")
+
+console.log(cookies)
+
+let count = 0 
+
+function increment() {
+    count = count + 1
+    cookies.innerText = count
+}
